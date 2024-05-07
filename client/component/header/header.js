dee355f61af0662360e87228c08cbb3255bdd978
Luca Cassanelli <lcassanelli.external@mcc.it>
2024-05-07 16:06:51 +0200

    style test per browser mobile

--- client/component/header/header.js
+++ client/component/header/header.js
@@ -5,10 +5,10 @@ import { Outlet, Link } from "react-router-dom";
 export default function Header(){
     return (
     <div className="header flex-center flex-row">
-        <span><Link to={`/`} className="link">Home</Link></span>
-        <span><Link to={`about`} className="link">About me</Link></span>
-        <span><Link to={`/`} className="link">Gallery</Link></span>
-        <span><Link to={`/`} className="link">Comics</Link></span> 
+        <Link to={`/`} className="link">Home</Link>
+        <Link to={`about`} className="link">About me</Link>
+        <Link to={`/`} className="link">Gallery</Link>
+        <Link to={`/`} className="link">Comics</Link> 
     </div>
     );
 }
