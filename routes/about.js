fc5d9c2c98cc9bfb5b6c7fcc01d4e04ab4898bc6
Luca Cassanelli <lcassanelli.external@mcc.it>
2024-05-21 13:49:46 +0200

    spostato e sperimentato il fetch da images nella pagina 'about'

--- routes/about.js
+++ routes/about.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+// Establish a Socket.io connection
+const socket = io();
+// Initialize our Feathers client application through Socket.io
+const client = feathers();
+
+client.configure(feathers.socketio(socket));
+
+async function getImages() {
+    const images = await client.service("images").find();
+    return images;
+}
 
 export default function About() {
+    const [response, setResponse] = useState(null);
+    //prende i dati dopo il caricamento della pagina
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await getImages();
+            setResponse(result);
+        };
+
+        fetchData();
+    }, []); // Aggiungi eventuali dipendenze qui
+    console.log(response);
     return (
         <>
             <h3>This is the About Me page</h3>
+            {response ? (
+                <p>Total: {response.total}</p>
+            ) : (
+                <p>Loading data...</p>
+            )}
         </>
-
     );
 }
