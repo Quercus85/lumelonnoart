6e7791f23c15450b7c7d6034f767f52b48776e3c
Luca Cassanelli <lcassanelli.external@mcc.it>
2024-08-19 17:34:45 +0200

    sperimentazione del recupero di immagini. Creazione di un hook per l' automatizzazione degli include delle tags dentro images.hooks

--- routes/about.js
+++ routes/about.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
+import { format } from "../src/utils/tags-of_images-json-formatter";
 
 // Establish a Socket.io connection
 const socket = io();
@@ -10,12 +11,14 @@ client.configure(feathers.socketio(socket));
 
 async function getImages() {
     const images = await client.service("images").find();
-    return images;
+    const jsonResponse = format(images);
+    return jsonResponse;
 }
 
 export default function About() {
     const [response, setResponse] = useState(null);
-    //prende i dati dopo il caricamento della pagina
+
+    // Prende i dati dopo il caricamento della pagina
     useEffect(() => {
         const fetchData = async () => {
             const result = await getImages();
@@ -24,15 +27,44 @@ export default function About() {
 
         fetchData();
     }, []); // Aggiungi eventuali dipendenze qui
+
     console.log(response);
+    if (response) {
+        console.log(response[0].image_url);
+    }
+
+    /*
+    TODO: il json delle tag contiene gli id invece delle sole descrizione. Fixalo
+    tags: Array(6) [ "1", "2020", "3", … ]
+        0: "1"
+        1: "2020"
+        2: "3"
+        3: "Sketch"
+        4: "4"
+        5: "Comic"
+    */
     return (
         <>
             <h3>This is the About Me page</h3>
             {response ? (
-                <p>Total: {response.total}</p>
+                response.map((item, index) => (
+                    <div key={index}>
+                        <p>Id: {item.id}</p>
+                        <p>Title: {item.image_name}</p>
+                        <a href={item.image_url} ><img src={item.image_thumb} /></a>
+                        <div>Tags:
+                            {item.tags.map((tag, index) => (
+                                <div key={index}>
+                                    <span>{tag}</span>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))
             ) : (
                 <p>Loading data...</p>
-            )}
+            )
+            }
         </>
     );
 }
--- src/services/images/images.hooks.js
+++ src/services/images/images.hooks.js
@@ -1,10 +1,35 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
-const findAllImages = require('../../hooks/find-all-images');
+//const { authenticate } = require('@feathersjs/authentication').hooks;
+
+const includeHook = async context => {
+  const app = context.app; // Accedi all'oggetto app dal contesto
+  const sequelizeClient = app.get('sequelizeClient');
+  const { tags, imagestags } = sequelizeClient.models;
+
+  // Verifica che i modelli siano definiti
+  if (!tags || !imagestags) {
+    throw new Error('Modelli tags o imagestags non trovati');
+  }
+
+  // Aggiungi il campo include alla query
+  context.params.sequelize = {
+    ...context.params.sequelize,
+    include: [{
+      model: tags,
+      as: 'tags',
+      through: {
+        model: imagestags,
+        attributes: []
+      }
+    }]
+  };
+
+  return context;
+};
 
 module.exports = {
   before: {
     all: [],
-    find: [],
+    find: [includeHook],
     get: [],
     create: [],
     update: [],
@@ -14,7 +39,6 @@ module.exports = {
 
   after: {
     all: [],
-    //find: [findAllImages()],
     find: [],
     get: [],
     create: [],
